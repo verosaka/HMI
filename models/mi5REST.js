606f1221a06b5d40d3c10ac3d62715f1c54b540e
Thomas Frei <mail@thomasfrei.de>
2015-09-15 14:14:50 +0200

    add manageRecipe in REST API

--- models/mi5REST.js
+++ models/mi5REST.js
@@ -2,6 +2,9 @@ var Q = require('q');
 var _ = require('underscore');
 var request = require('request');
 
+/**
+ *
+ */
 mi5rest = function() {
   // check if host is online
   var self = this;
@@ -16,7 +19,7 @@ mi5rest = function() {
   } catch(err){
     console.log('catch',err);
   }
-  
+
 };
 
 // This is needed, since mi5rest constructor is synchronous, its promised-
@@ -30,21 +33,55 @@ mi5rest.prototype.saveOrder = function(taskId, recipeId, userParameters){
   var self = instance;
 
   var order = {taskId: taskId,
-              recipeId: recipeId,
-              parameters: userParameters};
+    recipeId: recipeId,
+    parameters: userParameters};
 
   mi5Logger.info('new order saved to database:'+JSON.stringify(order,' '));
   return NewOrder.saveQ();
 };
 
+mi5rest.prototype.manageRecipe = function(recipeId, name, description, userparameters){
+  var self = instance;
+
+  var recipe = {recipeId: recipeId,
+    name: name,
+    description: description,
+    userparameters: userparameters};
+
+  var options = {
+    url: CONFIG.RESTHost+'/manageRecipe',
+    rejectUnauthoriyed: false,
+    formData: {
+      recipe: JSON.stringify({
+        recipeId: recipeId,
+        name: name,
+        description: description,
+        userparameters: userparameters
+      });
+    }
+  }
+  return Q.promise(function(resolve, reject){
+    request.post(options, function(err, httpRes){
+      if(!err){
+        mi5Logger.info('recipe was managed :'+JSON.stringify(httpRes,' '));
+        console.log(httpRes);
+        resolve(httpRes);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
+
 mi5rest.prototype._checkServer = function(host){
   var self = this;
   var deferred = Q.defer();
-  
+
+  // TODO: import mi5.itq.de certificate
+  // http://stackoverflow.com/questions/20082893/unable-to-verify-leaf-signature
   var options = {
     url: host,
-	rejectUnauthorized: false // TODO: import mi5.itq.de certificate
-	// http://stackoverflow.com/questions/20082893/unable-to-verify-leaf-signature
+	  rejectUnauthorized: false
   };
   request.get(options, function(err, res, body){
 	if(err) deferred.reject(err);
